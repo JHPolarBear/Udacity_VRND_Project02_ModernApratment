42f475bc324750c567033647b3e705059eab1cca
Jihwan Park <jhpark8904@gmail.com>
2016-11-30 15:11:41 +0900

    feat: First Upload for the Project Resources
    Upload the Project Resources for Modern Apratment VR scene.
      - Please Check the README.md

---
+++ Assets/VRND-Course2-Project/Scripts/GlobeRotateAnimation.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobeRotateAnimation : MonoBehaviour {
+    public string AnimationName;
+    public Animator stateMachine;
+
+    private bool created = false;
+
+    //bool variable to control globe rotation
+    private bool isActive = true;
+
+    void awake()
+    {
+        if (GvrViewer.Instance == null)
+        {
+            GvrViewer.Create();
+            created = true;
+        }
+    }
+
+    void Start()
+    {
+        if (created)
+        {
+            foreach (Camera c in GvrViewer.Instance.GetComponentsInChildren<Camera>())
+            {
+                c.enabled = false; // to use the Gvr SDK without adding cameras we have to disable them
+            }
+        }
+
+    }
+
+    void Update()
+    {
+        GvrViewer.Instance.UpdateState(); //need to update the data here otherwise we dont get mouse clicks; this is because we are automatically creating the GVRSDK (seems like a bug)
+        if (GvrViewer.Instance.Triggered)
+        {
+            //modify the code from TriggerAnimation.cs
+            
+            //change the value of the GlobeRotate Parameter in Sphere Animator
+            isActive = !isActive;
+
+            stateMachine.SetBool(AnimationName, isActive);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# Virtual Reality Nanodegree
+### Project for Course02 
+
+1. Name
+	* Jihwan Park
+
+2. The time required for this project
+	* About 3 hours
+
+3. The most Interesting Issue
+	* I could create various animation effect mixing basic transforms, and mecanim provides easy way to control animation.
+	  
+
+4. The most challenging Issue
+	* It takes most of the time setting up numbers of lights and regulating the values 
+	  to get acceptable lightmap. 
+	  Especially, spotligth rarely affect to ligth effect with its default intensity value(1) and angles, so
+	  I need to test sevearl time to get proper effect(Intensity: 3 / Angles: 60).
+	  
+	* Also, I found that when I set directional light in real time, It seems to olny affect to non-static object(ex. globe)
+	  but when I look into teaching materials, directional light affects to not only non-static objects, but also static objects.
+	  I tried to find out reason, but failed to get exact answer. so it will be grateful if you explain the reason.
